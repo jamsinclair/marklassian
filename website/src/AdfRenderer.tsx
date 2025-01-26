7c310d53cc7b0328503d9b8d54704d118924764d
Jamie Sinclair <jamsinclairnz@gmail.com>
2025-01-27 01:50:01 +0900

    try re-render a 2nd time

--- website/src/AdfRenderer.tsx
+++ website/src/AdfRenderer.tsx
@@ -10,13 +10,19 @@ function AdfRenderer({ document }: { document: any }) {
     if (hasReRendered) return;
 
     setTimeout(() => {
-        // Hack to force re-render.
-        // Code block rendering seems to be broken on first render.
-        // Atlaskit is a prime example of conway's law in action.
-        // Don't have time to fix this properly.
-        setKey((key) => key + 1);
-        hasReRendered = true;
+      // Hack to force re-render.
+      // Code block rendering seems to be broken on first render.
+      // Atlaskit is a prime example of conway's law in action.
+      // Don't have time to fix this properly.
+      setKey((key) => key + 1);
+      hasReRendered = true;
     }, 800);
+
+    setTimeout(() => {
+      // Try a second time in case the first one failed.
+      setKey((key) => key + 1);
+      hasReRendered = true;
+    }, 1500);
   }, []);
 
   return <ReactRenderer key={key} document={document} />;
