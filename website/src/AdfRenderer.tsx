170d8e47b196536af81e2177eb2564d9ccdca3f9
Jamie Sinclair <jamsinclairnz@gmail.com>
2025-01-27 00:48:35 +0900

    fix preview render bug in prod build

--- website/src/AdfRenderer.tsx
+++ website/src/AdfRenderer.tsx
@@ -1,7 +1,23 @@
 import { ReactRenderer } from "@atlaskit/renderer";
+import { useEffect, useState } from "react";
+
+let hasReRendered = false;
 
 function AdfRenderer({ document }: { document: any }) {
-  return <ReactRenderer document={document} />;
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    if (hasReRendered) return;
+
+    setTimeout(() => {
+        // Hack to force re-render.
+        // Code block rendering seems to be broken on first render.
+        setKey((key) => key + 1);
+        hasReRendered = true;
+    }, 150);
+  }, []);
+
+  return <ReactRenderer key={key} document={document} />;
 }
 
 export default AdfRenderer;
